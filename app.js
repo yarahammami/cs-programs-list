7dc3b758c3364f95771600ab36dc5f8c1194e490
yara hammami <yara.hammami13@gmail.com>
2022-01-02 19:36:54 +0200

    rest of the programs

--- app.js
+++
@@ -1,8 +0,0 @@
-function addProgram(){
- const programName = prompt("Enter the name of the program you wish to add: ");
- let listElt = document.createElement("li");
- listElt.textContent = programName;
- let list = document.getElementById("progList");
- list.appendChild(listElt);
-}
-
